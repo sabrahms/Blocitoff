bdf11d95c09fc6e771152d1b95f7adf12b04e291
Sam Abrahms <abrahms.sam@gmail.com>
2015-08-26 13:56:34 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,8 @@
+<!-- AngularJS -->
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+
+<!-- Firebase -->
+<script src="https://cdn.firebase.com/js/client/2.2.4/firebase.js"></script>
+
+<!-- AngularFire -->
+<script src="https://cdn.firebase.com/libs/angularfire/1.1.2/angularfire.min.js"></script>
